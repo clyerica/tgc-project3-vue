f275de87467aca14371c6df5a3b89be57e010ce4
clyerica <erica.chan@trentglobal.edu.sg>
2022-10-23 07:00:36 +0000

    made each recipe preview card a component

--- ez_makan/src/components/AllRecipes.vue
+++ ez_makan/src/components/AllRecipes.vue
@@ -1,43 +1,22 @@
 <template>
   <div class="container pt-4">
-    <div class="row row-cols-1 row-cols-md-3 row-cols-lg-4 g-3">
-      <div class="px-2" v-for="r in recipes" v-bind:key="r._id">
-      <div class="card">
-        <div class="card-header d-flex flex-wrap justify-content-evenly text-bg-secondary">
-          <span>
-            <span class="badge text-bg-info me-1">{{r.cuisine}}</span>
-          </span>
-          <span v-for="d in r.diet" v-bind:key="d">
-            <span class="badge text-bg-success me-1">{{d}}</span>
-          </span>
-        </div>
-        <div class="row g-0">
-          <img class="col-7 col-md-12" style="object-fit:cover; height:250px" v-bind:src="r.imageURL">
-          <div class=" col-5 col-md-12 d-flex flex-column">
-            <div class="card-body d-flex flex-column justify-content-center">
-              <h5 class="card-title text-center mb-3">{{r.title}}</h5>
-              <h6 class="card-subtitle text-center text-muted">Serves: {{r.serves}}</h6>
-            </div>
-            <div class="card-footer text-bg-warning text-center mt-auto">
-              Start Cooking
-              <font-awesome-icon icon="fa-solid fa-arrow-right-long" />
-            </div>
-          </div>
-        </div>
-      </div>          
-    </div>
-      
+    <div class="pe-md-5 pe-lg-0 row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4  g-3">
+      <RecipePreview v-for="r in recipes" v-bind:key="r._id" v-bind:recipe="r" class="px-2"/>        
     </div>
   </div>
 </template>
 
 <script>
 import axios from "axios";
+import RecipePreview from './RecipePreview.vue'
 
 const API_URL = "http://localhost:3000";
 
 export default {
  name: "RecipesData",
+ components:{
+    RecipePreview
+ },
  data: function () {
    return {
      recipes: [],
---
+++ ez_makan/src/components/RecipePreview.vue
@@ -0,0 +1,33 @@
+<template>
+<div>
+    <div class="card">
+        <div class="card-header d-flex flex-wrap justify-content-evenly text-bg-secondary">
+          <span>
+            <span class="badge text-bg-info me-1">{{recipe.cuisine}}</span>
+          </span>
+          <span v-for="d in recipe.diet" v-bind:key="d">
+            <span class="badge text-bg-success me-1">{{d}}</span>
+          </span>
+        </div>
+        <div class="row g-0">
+          <img class="col-7 col-md-12" style="object-fit:cover; height:250px" v-bind:src="recipe.imageURL">
+          <div class=" col-5 col-md-12 d-flex flex-column">
+            <div class="card-body d-flex flex-column justify-content-center">
+              <h5 class="card-title text-center mb-3">{{recipe.title}}</h5>
+              <h6 class="card-subtitle text-center text-muted">Serves: {{recipe.serves}}</h6>
+            </div>
+            <div class="card-footer text-bg-warning text-center mt-auto">
+              Start Cooking
+              <font-awesome-icon icon="fa-solid fa-arrow-right-long" />
+            </div>
+          </div>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+    export default{
+        props:['recipe']
+    }
+</script>
